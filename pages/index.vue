884e9d4f60cafcb93fc28afcc9d61d6a7c22217d
Lecrut <filip.lecrut12@gmail.com>
2024-03-13 13:34:40 +0100

    add no login parking option

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,3 @@
 <template>
-  <v-container fluid>
-    <v-row align="center" justify="center">
-      <v-col cols="12" sm="8" md="6">
-        <v-card class="elevation-12">
-          <v-toolbar color="primary" dark flat>
-            <v-toolbar-title>Aplikacja Parkingowa</v-toolbar-title>
-          </v-toolbar>
-          <v-card-text>
-            <v-container>
-              <v-row>
-                <v-col cols="12" sm="6" md="3">
-                  <v-text-field label="Numer rejestracyjny" outlined></v-text-field>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="primary">Zarezerwuj miejsce parkingowe</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+ witamy
 </template>
