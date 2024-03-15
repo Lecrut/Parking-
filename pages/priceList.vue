1a0d846dc4f8fccc89b4738ab45fd756372b3cc4
Lecrut <filip.lecrut12@gmail.com>
2024-03-15 21:52:26 +0100

    hf

--- pages/priceList.vue
+++ pages/priceList.vue
@@ -4,69 +4,67 @@
 </script>
 
 <template>
-  <v-container fluid>
     <v-sheet
       class="d-flex align-center justify-center flex-wrap text-center mx-auto my-10 px-4"
       elevation="4"
-      max-width="800"
+      max-width="1100"
       rounded
-  >
-    <v-row justify="center">
-      <v-col cols="12" sm="12" md="4">
-        <v-img
-            class="mx-auto my-5 elevation-5"
-            rounded="xl"
-            :width="250"
-            aspect-ratio="4/3"
-            cover
-            src="/ticketOne.jpg"
-        />
-      </v-col>
-      <v-col cols="12" sm="12" md="8">
-        <div class="d-flex flex-column align-center justify-center h-100 mx-2" >
-          <div class="text-h5 mb-2">
-            BILET JEDNORAZOWY
-          </div>
-
-          <p class="mb-1 mt-1">
-            Płać stałą sumę za każdą rozpoczętą godzinę. Nie wymaga logowania
-          </p>
-
-          <p class="text-h6 mt-2 mb-2 font-weight-bold" >
-            5 PLN / godzina
-          </p>
-          
-          <v-btn class="my-4" to="/noLoginParking">
-            Zakup bilet
-          </v-btn>
-        </div>
-      </v-col>
-    </v-row>
-    
-  </v-sheet>
-
-  <v-sheet
-      class="d-flex align-center justify-center flex-wrap text-center mx-auto my-10 px-4"
-      elevation="4"
-      max-width="800"
-      rounded
-  >
-    <v-row justify="center">
-      <v-col cols="12" sm="12" md="4">
-        <v-img
-            class="mx-auto my-5 elevation-5"
-            rounded="xl"
-            :width="250"
-            aspect-ratio="4/3"
-            cover
-            src="/ticketTwo.jpg"
-        />
-      </v-col>
-      <v-col cols="12" sm="12" md="8">
-        
+    >
+      <v-row justify="center">
+        <v-col cols="12" sm="12" md="6">
+          <v-img
+              class="mx-auto my-5 elevation-5"
+              rounded="xl"
+              :width="250"
+              aspect-ratio="4/3"
+              cover
+              src="/ticketOne.jpg"
+          />
+        </v-col>
+        <v-col cols="12" sm="12" md="6">
           <div class="d-flex flex-column align-center justify-center h-100 mx-2" >
             <div class="text-h5 mb-2">
-              BILET DZIENNY
+              Bilet jednorazowy
+            </div>
+
+            <p class="mb-1 mt-1">
+              Płać stałą sumę za każdą rozpoczętą godzinę. Nie wymaga logowania
+            </p>
+
+            <p class="text-h6 mt-2 mb-2 font-weight-bold" >
+              5 zł za godzinę
+            </p>
+          
+            <v-btn class="my-4" to="/noLoginParking">
+              Zakup bilet
+            </v-btn>
+          </div>
+        </v-col>
+      </v-row>
+    </v-sheet>
+
+    <v-sheet
+        class="d-flex align-center justify-center flex-wrap text-center mx-auto my-10 px-4"
+        elevation="4"
+        max-width="1100"
+        rounded
+    >
+      <v-row justify="center">
+        <v-col cols="12" sm="12" md="6">
+          <v-img
+              class="mx-auto my-5 elevation-5"
+              rounded="xl"
+              :width="250"
+              aspect-ratio="4/3"
+              cover
+              src="/ticketTwo.jpg"
+          />
+        </v-col>
+
+        <v-col cols="12" sm="12" md="6">
+          <div class="d-flex flex-column align-center justify-center h-100 mx-2" >
+            <div class="text-h5 mb-2">
+              Bilet dzienny
             </div>
 
             <p class="mb-1 mt-1">
@@ -74,28 +72,25 @@
             </p>
 
             <p class="text-h6 mt-2 mb-2 font-weight-bold">
-              20 PLN
+              20 zł
             </p>
             
             <v-btn class="my-4">
               Zakup bilet
             </v-btn>
-            
           </div>
       </v-col>
-      
     </v-row>
-    
   </v-sheet>
 
   <v-sheet
       class="d-flex align-center justify-center flex-wrap text-center mx-auto my-10 px-4"
       elevation="4"
-      max-width="800"
+      max-width="1100"
       rounded
   >
     <v-row justify="center">
-      <v-col cols="12" sm="12" md="4">
+      <v-col cols="12" sm="12" md="6">
         <v-img
             class="mx-auto my-5 elevation-5"
             rounded="xl"
@@ -106,8 +101,7 @@
         />
       </v-col>
 
-      <v-col cols="12" sm="12" md="8">
-        
+      <v-col cols="12" sm="12" md="6">
           <div class="d-flex flex-column align-center justify-center h-100 mx-2" >
             <div class="text-h5 mb-2">
               BILET TYGODNIOWY
@@ -136,12 +130,12 @@
   <v-sheet
       class="d-flex align-center justify-center flex-wrap text-center mx-auto my-10 px-4"
       elevation="4"
-      max-width="800"
+      max-width="1100"
       rounded
   >
     <v-row justify="center">
 
-      <v-col cols="12" sm="12" md="4">
+      <v-col cols="12" sm="12" md="6">
         <v-img
             class="mx-auto my-5 elevation-5"
             rounded="xl"
@@ -151,11 +145,10 @@
             src="/ticketFour.jpg"
         />
       </v-col>
-      <v-col cols="12" sm="12" md="8">
-        
+      <v-col cols="12" sm="12" md="6">
           <div class="d-flex flex-column align-center justify-center h-100 mx-2" >
             <div class="text-h5 mb-2">
-              BILET MIESIĘCZNY
+              Bilet miesięczny
             </div>
 
             <p class="mb-1 mt-1">
@@ -163,23 +156,16 @@
             </p>
 
             <p class="text-h6 mt-2 mb-2 font-weight-bold">
-              250 PLN
+              250 zł
             </p>
             
             <v-btn class="my-4">
               Zakup bilet
             </v-btn>
           </div>
-        
       </v-col>
-
-      
-      
     </v-row>
-    
   </v-sheet>
-
-  </v-container>
 </template>
 
 <style scoped>
