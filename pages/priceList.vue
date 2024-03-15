8d3a417d7d72d3d386523619534c1674427a7132
Piotr Jasinski <piotr.jasinski2002@gmail.com>
2024-03-15 19:20:33 +0100

    poprawa widoku

--- pages/priceList.vue
+++ pages/priceList.vue
@@ -8,7 +8,7 @@
     <v-sheet
       class="d-flex align-center justify-center flex-wrap text-center mx-auto my-10 px-4"
       elevation="4"
-      max-width="900"
+      max-width="800"
       rounded
   >
     <v-row justify="center">
@@ -24,7 +24,7 @@
       </v-col>
       <v-col cols="12" sm="12" md="8">
         <div class="d-flex flex-column align-center justify-center h-100 mx-2" >
-          <div class="text-h3 mb-2">
+          <div class="text-h5 mb-2">
             BILET JEDNORAZOWY
           </div>
 
@@ -32,7 +32,7 @@
             Płać stałą sumę za każdą rozpoczętą godzinę. Nie wymaga logowania
           </p>
 
-          <p class="text-h3 mt-2 mb-2">
+          <p class="text-h6 mt-2 mb-2 font-weight-bold" >
             5 PLN / godzina
           </p>
           
@@ -48,33 +48,10 @@
   <v-sheet
       class="d-flex align-center justify-center flex-wrap text-center mx-auto my-10 px-4"
       elevation="4"
-      max-width="900"
+      max-width="800"
       rounded
   >
     <v-row justify="center">
-      
-      <v-col cols="12" sm="12" md="8">
-        <div class="d-flex flex-column align-center justify-center h-100 mx-2" >
-          <div class="d-flex flex-column align-center justify-center h-100 mx-2" >
-            <div class="text-h3 mb-2">
-              BILET DZIENNY
-            </div>
-
-            <p class="mb-1 mt-1">
-              Bilet obowiązujący cały dzień od 00:00 do 23:59. Wymaga logowania.
-            </p>
-
-            <p class="text-h3 mt-2 mb-2">
-              20 PLN
-            </p>
-            
-            <v-btn class="my-4">
-              Zakup bilet
-            </v-btn>
-            
-          </div>
-        </div>
-      </v-col>
       <v-col cols="12" sm="12" md="4">
         <v-img
             class="mx-auto my-5 elevation-5"
@@ -85,6 +62,28 @@
             src="/ticketTwo.jpg"
         />
       </v-col>
+      <v-col cols="12" sm="12" md="8">
+        
+          <div class="d-flex flex-column align-center justify-center h-100 mx-2" >
+            <div class="text-h5 mb-2">
+              BILET DZIENNY
+            </div>
+
+            <p class="mb-1 mt-1">
+              Bilet obowiązujący cały dzień od 00:00 do 23:59. Wymaga logowania.
+            </p>
+
+            <p class="text-h6 mt-2 mb-2 font-weight-bold">
+              20 PLN
+            </p>
+            
+            <v-btn class="my-4">
+              Zakup bilet
+            </v-btn>
+            
+          </div>
+      </v-col>
+      
     </v-row>
     
   </v-sheet>
@@ -92,7 +91,7 @@
   <v-sheet
       class="d-flex align-center justify-center flex-wrap text-center mx-auto my-10 px-4"
       elevation="4"
-      max-width="900"
+      max-width="800"
       rounded
   >
     <v-row justify="center">
@@ -108,9 +107,9 @@
       </v-col>
 
       <v-col cols="12" sm="12" md="8">
-        <div class="d-flex flex-column align-center justify-center h-100 mx-2" >
+        
           <div class="d-flex flex-column align-center justify-center h-100 mx-2" >
-            <div class="text-h3 mb-2">
+            <div class="text-h5 mb-2">
               BILET TYGODNIOWY
             </div>
 
@@ -118,7 +117,7 @@
               Bilet obowiązujący na 7 dni. Wymaga logowania.
             </p>
 
-            <p class="text-h3 mt-2 mb-2">
+            <p class="text-h6 mt-2 mb-2 font-weight-bold">
               100 PLN
             </p>
             
@@ -127,7 +126,7 @@
             </v-btn>
             
           </div>
-        </div>
+        
       </v-col>
       
     </v-row>
@@ -137,32 +136,10 @@
   <v-sheet
       class="d-flex align-center justify-center flex-wrap text-center mx-auto my-10 px-4"
       elevation="4"
-      max-width="900"
+      max-width="800"
       rounded
   >
     <v-row justify="center">
-
-      <v-col cols="12" sm="12" md="8">
-        <div class="d-flex flex-column align-center justify-center h-100 mx-2" >
-          <div class="d-flex flex-column align-center justify-center h-100 mx-2" >
-            <div class="text-h3 mb-2">
-              BILET MIESIĘCZNY
-            </div>
-
-            <p class="mb-1 mt-1">
-              Bilet obowiązujący na 30 dni.  Wymaga logowania.
-            </p>
-
-            <p class="text-h3 mt-2 mb-2">
-              250 PLN
-            </p>
-            
-            <v-btn class="my-4">
-              Zakup bilet
-            </v-btn>
-          </div>
-        </div>
-      </v-col>
 
       <v-col cols="12" sm="12" md="4">
         <v-img
@@ -174,6 +151,29 @@
             src="/ticketFour.jpg"
         />
       </v-col>
+      <v-col cols="12" sm="12" md="8">
+        
+          <div class="d-flex flex-column align-center justify-center h-100 mx-2" >
+            <div class="text-h5 mb-2">
+              BILET MIESIĘCZNY
+            </div>
+
+            <p class="mb-1 mt-1">
+              Bilet obowiązujący na 30 dni.  Wymaga logowania.
+            </p>
+
+            <p class="text-h6 mt-2 mb-2 font-weight-bold">
+              250 PLN
+            </p>
+            
+            <v-btn class="my-4">
+              Zakup bilet
+            </v-btn>
+          </div>
+        
+      </v-col>
+
+      
       
     </v-row>
     
