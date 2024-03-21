5ef67dc243cd186cbc09b81a104dcea5de66f1f1
Piotr Jasinski <piotr.jasinski2002@gmail.com>
2024-03-21 18:46:53 +0100

    wstępny widok

--- pages/client/index.vue
+++ pages/client/index.vue
@@ -2,14 +2,63 @@
 import NavBarUser from '~/components/navBars/navBarUser.vue'
 
 const auth = useAuthStore()
+
+// const cars = [
+//   {
+//     value: 'bmw',
+//     registration: 'EZG 12345',
+//   },
+//   {
+//     value: 'mercedes',
+//     registration: 'GDA 33333',
+//   },
+// ]
 </script>
 
 <template>
   <NavBarUser />
-  strona glowa usera
-  <div>
-    Witaj {{ auth.user?.name }}
-  </div>
+  <v-sheet
+    class="d-flex align-center justify-center flex-wrap text-center mx-auto my-10 px-4" elevation="4"
+    max-width="1100" rounded
+  >
+    <v-row justify="center">
+      <v-col>
+        <div class="text-h2 ma-3">
+          Witaj, {{ auth.user?.name }}!
+        </div>
+
+        <v-row>
+          <v-col>
+            <p class="text-h5">
+              Zakup bilet jednorazowy
+            </p>
+            <v-btn class="my-4">
+              Kup bilet
+            </v-btn>
+          </v-col>
+
+          <v-col>
+            <p class="text-h5">
+              Zakup bilet miesięczny
+            </p>
+
+            <v-btn class="my-4">
+              Zakup bilet
+            </v-btn>
+          </v-col>
+
+          <v-col>
+            <p class="text-h5">
+              Twoje bilety
+            </p>
+            <v-btn class="my-4">
+              Zakup bilet
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-sheet>
 </template>
 
 <style scoped>
