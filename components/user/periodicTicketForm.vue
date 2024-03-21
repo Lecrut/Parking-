fc4ad58915b9c7286576b3d40a87ee1a211f0e6f
Piotr Jasinski <piotr.jasinski2002@gmail.com>
2024-03-21 19:26:47 +0100

    dodanie wstępnego widoku dialogowego z wyborem typu biletu i miejsca

---
+++ components/user/periodicTicketForm.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+const props = defineProps < {
+  isShow: boolean
+} > ()
+
+const emit = defineEmits < {
+  (e: 'onClose'): void
+}> ()
+
+const { isShow } = toRefs(props)
+const isShowRef = ref < boolean > ()
+
+function close() {
+  emit('onClose')
+}
+
+const ticketTypes = ['Dzienny', 'Tygodniowy', 'Miesięczny']
+
+watch(isShow, () => isShowRef.value = isShow.value)
+</script>
+
+<template>
+  <v-dialog max-width="1000px" :model-value="isShowRef" scrollable @update:model-value="close">
+    <v-card>
+      <v-card-title>
+        Kup bilet okresowy
+      </v-card-title>
+
+      <v-combobox label="Okres" :items="ticketTypes" variant="underlined" />
+      <v-combobox label="Miejsce" :items="['1', '2', '3']" variant="underlined" />
+    </v-card>
+  </v-dialog>
+</template>
+
+<style scoped></style>
--- pages/client/index.vue
+++ pages/client/index.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts">
 import NavBarUser from '~/components/navBars/navBarUser.vue'
+import periodicTicketForm from '~/components/user/periodicTicketForm.vue'
+
+const periodicTicketFlag = ref(false)
+
+function changeTicketFlag() {
+  periodicTicketFlag.value = !periodicTicketFlag.value
+}
 
 const auth = useAuthStore()
 
@@ -19,15 +26,15 @@ const auth = useAuthStore()
   <NavBarUser />
   <v-sheet
     class="d-flex align-center justify-center flex-wrap text-center mx-auto my-10 px-4" elevation="4"
-    max-width="1100" rounded
+    max-width="1400" rounded
   >
     <v-row justify="center">
-      <v-col>
+      <v-col class="mt-8">
         <div class="text-h2 ma-3">
           Witaj, {{ auth.user?.name }}!
         </div>
 
-        <v-row>
+        <v-row class="mt-8">
           <v-col>
             <p class="text-h5">
               Zakup bilet jednorazowy
@@ -42,8 +49,8 @@ const auth = useAuthStore()
               Zakup bilet miesięczny
             </p>
 
-            <v-btn class="my-4">
-              Zakup bilet
+            <v-btn class="my-4" @click="periodicTicketFlag = true">
+              Kup bilet
             </v-btn>
           </v-col>
 
@@ -52,13 +59,14 @@ const auth = useAuthStore()
               Twoje bilety
             </p>
             <v-btn class="my-4">
-              Zakup bilet
+              Twoje bilety
             </v-btn>
           </v-col>
         </v-row>
       </v-col>
     </v-row>
   </v-sheet>
+  <periodicTicketForm :is-show="periodicTicketFlag" @on-close="changeTicketFlag" />
 </template>
 
 <style scoped>
