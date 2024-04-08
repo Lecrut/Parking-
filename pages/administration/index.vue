168832c8b5a3e5424ce6c25119d982f4fb9c8068
Piotr Jasinski <piotr.jasinski2002@gmail.com>
2024-04-08 15:17:00 +0200

    hot fix

--- pages/administration/index.vue
+++ pages/administration/index.vue
@@ -2,6 +2,7 @@
 import NavBarAdmin from '~/components/navBars/navBarAdmin.vue'
 import parkingPlace from '~/components/administration/parkingPlace.vue'
 import ticket from '~/components/user/ticket.vue'
+import type { IEvent } from '~/models/Event'
 
 definePageMeta({
   middleware: ['admin-page-guard'],
@@ -10,8 +11,25 @@ definePageMeta({
 const ticketStore = useTicketStore()
 const { validTickets } = storeToRefs(ticketStore)
 
-onMounted(() => {
-  ticketStore.fetchAllValidTickets()
+const placeTable = Array.from({ length: 50 }, (_, i) => ({
+  placeNum: i,
+  placeTicket: null as IEvent | null,
+  isFree: true,
+}))
+
+function createPlaceTable() {
+  for (let i = 0; i < placeTable.length; i++) {
+    validTickets.value.forEach((bilet: IEvent) => {
+      if (bilet.fieldNum === i) {
+        placeTable[i].placeTicket = bilet
+        placeTable[i].isFree = false
+      }
+    })
+  }
+}
+
+onMounted(async () => {
+  await ticketStore.fetchAllValidTickets().then(createPlaceTable)
 })
 </script>
 
@@ -27,23 +45,42 @@ onMounted(() => {
         </v-col>
       </v-row>
 
-      <v-row>
-        <v-col v-for="(ticket, index) in validTickets" :key="index" :item="ticket" cols="4">
-          <ticket :ticket />
+      <v-row justify="center">
+        <v-col v-for="parkingSpot in 50" :key="parkingSpot" cols="2">
+          <v-card>
+            <v-card-title class="text-center">
+              Miejsce {{ parkingSpot }}
+            </v-card-title>
+          </v-card>
         </v-col>
       </v-row>
 
-      <v-slide-group class="pa-4" selected-class="bg-primary" show-arrows>
-        <v-slide-group-item v-for="n in 60" :key="n" v-slot="{ isSelected, toggle, selectedClass }">
-          <v-card class="ma-4" :class="[selectedClass]" color="grey-lighten-1" height="200" width="100" @click="toggle">
-            <div class="d-flex fill-height align-center justify-center">
-              <v-scale-transition>
-                <v-icon v-if="isSelected" color="white" icon="mdi-close-circle-outline" size="48" />
-              </v-scale-transition>
+      <!-- <v-row>
+        <v-col v-for="(ticket, index) in validTickets" :key="index" :item="ticket" cols="4">
+          <ticket :ticket />
+        </v-col>
+      </v-row> -->
+
+      <!-- <v-slide-group class="pa-4" selected-class="bg-primary" show-arrows>
+        <v-slide-group-item v-for="n in 50" :key="n" v-slot="{ isSelected, toggle, selectedClass }">
+          <v-card
+            v-if="placeTable[n].placeTicket === null" class="ma-4" :class="[selectedClass]" color="grey-lighten-1"
+            height="200" width="100" @click="toggle"
+          />
+
+          <v-card>
+            <div>
+              {{ placeTable[n].isFree }}
             </div>
           </v-card>
+
+          <div class="d-flex fill-height align-center justify-center">
+            <v-scale-transition>
+              <v-icon v-if="isSelected" color="white" icon="mdi-close-circle-outline" size="48" />
+            </v-scale-transition>
+          </div>
         </v-slide-group-item>
-      </v-slide-group>
+      </v-slide-group> -->
     </v-container>
   </v-app>
 </template>
