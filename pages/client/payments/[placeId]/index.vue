f1464c7fc9db92ec8865e6ca6089d3c1a126726b
Lecrut <filip.lecrut12@gmail.com>
2024-04-19 22:03:35 +0200

    add place to pay for ticket

---
+++ pages/client/payments/[placeId]/index.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+definePageMeta({
+  middleware: ['user-page-guard'],
+})
+
+useHead({
+  title: 'Płatność - Parking+',
+})
+
+const route = useRoute()
+
+const placeId = ref(route.params.placeId || '')
+</script>
+
+<template>
+  <v-sheet
+      class="d-flex align-center justify-center flex-wrap text-center mx-auto my-10 px-4"
+      elevation="4"
+      max-width="1100"
+      rounded
+  >
+    Płatność {{placeId}}
+  </v-sheet>
+</template>
